85d1c62cc9b07dca5d33ed008b62e336865c15aa
tamunosaki jonah <tamunosakijonah@gmail.com>
2023-02-20 16:10:06 +0000

    control task

---
+++ index.js
@@ -0,0 +1,21 @@
+let scienceStudent = 'physics,chemistry,biology,technicalDrawing'
+let socialScienceStudent ='Accounting, Commerce, Marketing, Geography'
+let artsStudent ='Government, Economics, Literature, History'
+let generalSubjects = 'English, Mathematics'
+//edit the students value to suit you.you can enter scienceStudent or socialScienceStudent or artStudent
+let students = artsStudent
+
+if(students==artsStudent){
+    console.log('you will be learning' + ' ' + artsSubjects +' ' + generalSubjects)
+}else if(students==scienceStudent ){
+    console.log('you will be learning' + ' ' + scienceStudents +' ' + generalSubjects)
+}else if(students==socialScienceStudent){
+    console.log('you will be learning' + ' ' + socialScienceSubjects +' ' + generalSubjects)
+}else{
+    console.log('you will be learning' + ' ' + generalSubjects)
+}
+
+
+
+
+
